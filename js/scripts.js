eec5d8366546e53b23e02e43091b7035d63e2055
Kacper Wołoszyn <budzio161@gmail.com>
2018-01-12 21:26:56 +0100

    Change content

--- js/scripts.js
+++ js/scripts.js
@@ -17,18 +17,18 @@ function searchCountries() {
 function showCountriesList(resp) {
     countriesList.empty();
     resp.forEach(function (item) {
-        
+
         var mainList = $('<li id="containers">');
         var list = $('<ul id="secoundList">');
         var img = $('<img>').attr('src', item.flag);
-        
+
         $('<li>').text("Country: " + item.name).appendTo(list);
         $('<li>').text("Region: " + item.subregion).appendTo(list);
         $('<li>').text("Capital: " + item.capital).appendTo(list);
         $('<li>').text("Time zone: " + item.timezones).appendTo(list);
         $('<li>').text("Population: " + item.population + " people").appendTo(list);
         $('<li>').text("Border neighbors: " + item.borders).appendTo(list);
-        
+
         $('<li>').append(img).appendTo(list);
         $(list).appendTo(mainList);
         $(mainList).appendTo(countriesList);
